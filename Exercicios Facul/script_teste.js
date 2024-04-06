d5851be8daae389480302d5cbf123f856e54564d
MATHEUS HENRIQUE RIBEIRO SILVA <mhrs2002@gmail.com>
2024-04-06 09:27:45 -0300

    Create script_teste.js

---
+++ Exercicios Facul/script_teste.js
@@ -0,0 +1,9 @@
+function clicar(){
+ let frase = document.getElementById("fraseteste")
+ frase.innerHTML = "Deu certo!"
+}
+
+function voltar() {
+    let limpo = document.getElementById("fraseteste")
+    limpo.innerHTML = "Frase de teste!"
+}
